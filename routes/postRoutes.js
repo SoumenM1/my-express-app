a131b55f9e9ec43f63b09e23b2510451fd900ae7
soumen <soumen@idreameducation.org>
2024-03-21 14:17:39 +0530

    add post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const getPost = require('../middleware/getPost');
 const authMiddleware = require('../middleware/authMiddleware')
 // Routes for posts
 router.post('/posts', authMiddleware,postController.createPost);
-router.get('/posts', authMiddleware,postController.getAllPosts);
+router.get('/posts',postController.getAllPosts);
 router.get('/posts',authMiddleware,getPost, postController.getPostById);
 router.patch('/posts',authMiddleware,getPost, postController.updatePost);
 router.delete('/posts',authMiddleware,getPost, postController.deletePost);
